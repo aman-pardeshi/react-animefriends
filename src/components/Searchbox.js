94b0667a6babafec4207e356cf77e913b1cb9f17
Aman Pardeshi <amanpardeshi@yahoo.co.in>
2021-08-21 16:22:56 +0530

    adding project files

---
+++ src/components/Searchbox.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../containers/App";
+
+const Searchbox = ({ searchfield, searchChange }) => {
+  return (
+    <div className="searchSection">
+      <input
+        className="pa3 ba b--green bg-lightest-blue br4"
+        type="search"
+        placeholder="Search Anime Characters"
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
+
+export default Searchbox;
